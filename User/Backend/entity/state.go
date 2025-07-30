48521b870f861c4df735ba09ed64400582b8a7ad
Jibx24 <jib.panisra11@gmail.com>
2025-07-30 22:38:23 +0700

    เพิ่มentity

--- User/Backend/entity/state.go
+++ User/Backend/entity/state.go
@@ -5,7 +5,10 @@ import "gorm.io/gorm"
 type State struct {
 	gorm.Model
 	Name  string `gorm:"type:varchar(100);unique" json:"name"` 
-	Color string `gorm:"type:varchar(20)" json:"color"`        
+	Color string `gorm:"type:varchar(20)" json:"color"`  
+
+	UserID uint  `json:"user_id"`// 👈 FK ไปยัง role.id
+	Users  Users `gorm:"foreignKey:UserID"`  
 
 	// ความสัมพันธ์กับ Petition
 	Petitions []Petition `gorm:"foreignKey:StateID" json:"petitions"` // Relationship with the Petition model
